b609cac5d22f4a59e36e60718ceb80baca893a13
Fatima-Zohra Nazih <61879651+fnazih@users.noreply.github.com>
2021-01-04 13:30:35 +0100

    first commit | plotting all traces

---
+++ code.m
@@ -0,0 +1,17 @@
+start = 1;
+finish = 300;
+BaseName = 'Traces_EM\20201208-0001 (';
+matrice = [300, 5000];
+
+for i = start:finish
+    FileName = [BaseName, num2str(i), ').mat'];
+    Data = load (FileName);
+    for j = 1:5000
+       matrice(i, j) = Data.A(j);
+    end
+    plot(Data.A);
+    hold on;
+end
+
+disp(matrice);
+
